fa6f3c0198ce23099db807c9aaa87b43458c2ebe
PinoGabriel <pinogabriel01@gmail.com>
2023-10-31 16:20:37 +0100

    added cicle for

--- js/main.js
+++ js/main.js
@@ -6,15 +6,18 @@ let myMail = prompt("inserisci la tua mail:", "pinogabriel@gmail.com");
 
 const mailList = ["pinogabriel@gmail.com", "luca@gmail.com", "marco@gmail.com"];
 
+
 for (let i = 0; i < mailList.length; i++) {
-    const element = mailList[i];
-    console.log(element)
+    console.log(mailList[i])
     
-    if (myMail == element) {
+    if (myMail == mailList[i]) {
+        console.log("funziona");
         eMail.innerHTML = "funziona"
     } else {
+        console.log("non funziona");
         eMail.innerHTML = "non funziona"
     }
 }
 
 console.log("email da sola ", mailList[2]);
+console.log(mailList.length);
